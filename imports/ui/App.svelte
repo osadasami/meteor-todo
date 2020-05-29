41073fc8457195f28b387ca1389785f24618c17a
osadasami <osadasami@gmail.com>
2020-05-29 15:02:33 +0500

    can filter tasks and see incomplete tasks count

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,36 +1,54 @@
 <script>
-  import { useTracker } from 'meteor/rdb:svelte-meteor-data';
+	import { useTracker } from 'meteor/rdb:svelte-meteor-data';
 	import Task from './Task.svelte';
-  import { Tasks } from '../api/tasks.js'
+	import { Tasks } from '../api/tasks.js'
 
-  let newTask = ''
+	let newTask = ''
+	let hideCompleted = false;
+	let tasks
 
-	$: tasks = useTracker(() => Tasks.find({}, { sort: { createdAt: -1 } }).fetch());
+	const taskStore = Tasks.find({}, { sort: { createdAt: -1 } });
+
+	$: incompleteCount = useTracker(() => Tasks.find({ checked: { $ne: true } }).count());
+
+	$: {
+		tasks = $taskStore;
+		if (hideCompleted) {
+				tasks = tasks.filter(task => !task.checked);
+		}
+	};
 
 	function handleSubmit(event) {
-    Tasks.insert({
-      text: newTask,
-      createdAt: new Date()
-    });
+		Tasks.insert({
+			text: newTask,
+			createdAt: new Date()
+		});
 
-    newTask = '';
-  }
+		newTask = '';
+	}
 </script>
  
  
 <div class="container">
 	<header>
-		<h1>Todo List</h1>
+		<h1>Todo List {$incompleteCount}</h1>
+		<label className="hide-completed">
+			<input
+				type="checkbox"
+				bind:checked={hideCompleted}
+			/>
+			Hide Completed Tasks
+		</label>
 		<form class="new-task" on:submit|preventDefault={handleSubmit}>
-		  <input
-		    type="text"
-		    placeholder="Type to add new tasks"
-		    bind:value={newTask}
-		  />
+			<input
+				type="text"
+				placeholder="Type to add new tasks"
+				bind:value={newTask}
+			/>
 		</form>
 	</header>
 	<ul>
-	{#each $tasks as task}
+	{#each tasks as task}
 		<Task
 			key={task._id}
 			task={task}
