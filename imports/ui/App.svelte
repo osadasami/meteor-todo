f3a003e7527a85922d3956db156ae15a9adff584
osadasami <osadasami@gmail.com>
2020-05-29 14:40:14 +0500

    can see tasks from database

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,14 +1,9 @@
 <script>
+  import { useTracker } from 'meteor/rdb:svelte-meteor-data';
 	import Task from './Task.svelte';
-	
-	function getTasks() {
-		return [
-			{ _id: 1, text: "This is task 1" },
-			{ _id: 2, text: "This is task 2" },
-			{ _id: 3, text: "This is task 3" }
-		];
-	}
- 
+  import { Tasks } from '../api/tasks.js'
+
+	$: tasks = useTracker(() => Tasks.find({}).fetch());
 </script>
  
  
@@ -17,7 +12,7 @@
 		<h1>Todo List</h1>
 	</header>
 	<ul>
-	{#each getTasks() as task}
+	{#each $tasks as task}
 		<Task
 			key={task._id}
 			task={task}
