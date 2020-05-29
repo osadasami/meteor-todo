a7d591c1aafc11fa2c87b75137bb9d5fa665c27b
osadasami <osadasami@gmail.com>
2020-05-29 14:41:52 +0500

    can add tasks

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -3,13 +3,31 @@
 	import Task from './Task.svelte';
   import { Tasks } from '../api/tasks.js'
 
+  let newTask = ''
+
 	$: tasks = useTracker(() => Tasks.find({}).fetch());
+
+	function handleSubmit(event) {
+    Tasks.insert({
+      text: newTask,
+      createdAt: new Date()
+    });
+
+    newTask = '';
+  }
 </script>
  
  
 <div class="container">
 	<header>
 		<h1>Todo List</h1>
+		<form class="new-task" on:submit|preventDefault={handleSubmit}>
+		  <input
+		    type="text"
+		    placeholder="Type to add new tasks"
+		    bind:value={newTask}
+		  />
+		</form>
 	</header>
 	<ul>
 	{#each $tasks as task}
