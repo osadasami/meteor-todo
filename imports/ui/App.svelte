9e4df70f07eaf373e10627c9d3b34e73a1c19d47
osadasami <osadasami@gmail.com>
2020-05-29 14:35:22 +0500

    can see tasks list

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,5 +1,27 @@
 <script>
-	let greeting = 'Hello World'
+	import Task from './Task.svelte';
+	
+	function getTasks() {
+		return [
+			{ _id: 1, text: "This is task 1" },
+			{ _id: 2, text: "This is task 2" },
+			{ _id: 3, text: "This is task 3" }
+		];
+	}
+ 
 </script>
-
-<h1>{greeting}</h1>
+ 
+ 
+<div class="container">
+	<header>
+		<h1>Todo List</h1>
+	</header>
+	<ul>
+	{#each getTasks() as task}
+		<Task
+			key={task._id}
+			task={task}
+		/>
+	{/each}
+	</ul>
+</div>
